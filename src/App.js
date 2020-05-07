eb700ed46f0ecb027bbc2d472c63e239a3af5f94
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-05-06 22:06:37 -0500

    switch to reactstrap for better documention and react support

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
 import React from 'react';
 import './App.css';
+import Index from '../src/Components/Index/index';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Index />;
 }
 
 export default App;
--- src/Components/Index/index.js
+++ src/Components/Index/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
-import "./style.css";
+import {Container, Row, Col, Nav, NavItem, NavLink} from 'reactstrap';
 
-function Index () {
-
+function Index() {
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col md={4}>
+            <h3>Emily Taylor</h3>
+          </Col>
+          <Col md={8}>
+          <div>
+      <p>List Based</p>
+      <Nav>
+        <NavItem>
+          <NavLink href="#">Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#">Another Link</NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink disabled href="#">Disabled Link</NavLink>
+        </NavItem>
+      </Nav>
+      <hr />
+    </div>
+          </Col>
+      </Row>
+    </Container>
+    </div>
+  )
 }
 
 export default Index;
