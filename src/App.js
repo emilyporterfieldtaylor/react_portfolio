b10a07675c5abdc40dcb2bb543a96b49a9d27805
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-05-07 09:35:40 -0500

    add react-router-dom to create the routing for my different pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from "../src/Components/Header/Header"
 import Index from '../src/Components/Index/index';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return <Index />;
+  return (
+    <Router>  
+    <Header/>
+    <Switch>
+    {/* this ensures that either / or home can be used to bring up homepage w/o breaking*/}
+    <Route exact path={["/", "/index"]}><Index /></Route>
+    </Switch>
+  </Router>
+  )
 }
 
 export default App;
--- src/Components/Index/index.js
+++ src/Components/Index/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from "../Header/Header"
 import japanSquare from "../Images/japanSquare.jpg"
 import './style.css';
 import {Container, Row, Col} from 'reactstrap';
@@ -7,7 +6,6 @@ import {Container, Row, Col} from 'reactstrap';
 function Index() {
   return (
     <div>
-      < Header />
         <Container>
           <Row>
             <Col md={6}>
@@ -16,13 +14,11 @@ function Index() {
             <Col md={6}>
             <p class="flow-text">This is a third attempt at a portfolio for the UT Full Stack coding Bootcamp. This
                 website utilizes React and Reactstrap.
-                The goal was to take our original portfolio and make it more unique to show off what we've learned.
-                Please visit the portfolio page to see other projects I have worked on. The projects feature technologies like Node, Sequelize, Heroku and many more. Currently, my Contact page is
-                under construction.</p>
+                The goal was to take our portfolio and update it using react. The second iteration of my portfolio featured the Materialize framework which I found did not work as with with react as Reactstrap.
+                Please visit the portfolio page to see other projects I have worked on. The projects feature technologies like Node, Sequelize, Heroku and many more. Currently, my Contact page is under construction.</p>
               <p class="flow-text">I recently moved to Austin but I've lived in the Austin area for the past 8 years. I
-                have two kitties,
-                Calcifer and Sofie, named after characters from one of my favorite movies, Howl's Moving Castle. I try to
-                spend as much time outside as my laziness will allow. I like to hunt for mushrooms or nice little shady
+                have two kitties, Calcifer and Sofie, named after characters from one of my favorite movies, Howl's Moving Castle. I try to
+                spend as much time outside as Covid-19 will allow. I like to hunt for mushrooms or nice little shady
                 spots to read and draw. I also really enjoy playing D&D and video games. I'm just a lady trying to
                 further her education since I'm not doing a damn thing with my
                 Psychology degree. I've worked in IT adjacent jobs for the past few years and I'm ready to actually do
